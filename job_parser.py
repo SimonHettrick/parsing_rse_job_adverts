5822ebab812cc5d71557f7d9f0ea454fb34bd6af
Simon Hettrick <s.hettrick@software.ac.uk>
2020-02-25 20:48:50 +0000

    Parser now appears to be working

--- job_parser.py
+++ job_parser.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 from bs4 import BeautifulSoup
+import numpy as np
 import glob
 import os
 import re
@@ -11,6 +12,10 @@ DATASTORE = './testjobs/'
 
 
 def find_files():
+    """
+    Goes through the DATASTORE directory and collects names of all the files
+    :return: a list of all files
+    """
 
     list_of_adverts = glob.glob(DATASTORE + '*')
 
@@ -20,7 +25,7 @@ def find_files():
 def import_csv_to_df(location, filename):
     """
     Imports a csv file into a Pandas dataframe
-    :params: an xls file and a sheetname from that file
+    :params: an csv file and a filename from that file
     :return: a df
     """
 
@@ -38,20 +43,69 @@ def export_to_csv(df, location, filename, index_write):
 
 
 def read_html(list_of_adverts):
+    """
+    Goes through the list of files in the DATASTORE dir, finds the ones related to job adverts
+    using a regex, pulls out the <h1> tag, because that's generally the job title, then stores all
+    the job titles in a column of a dataframe
+    :param list_of_adverts: a list of the job advert filenames
+    :return: a df with a column of job titles from the filenames
+    """
 
-    for current_add in list_of_adverts:
-        filename = os.path.basename(current_add)
+    big_data_list = []
+
+    # Setting up html tag remover
+    clean = re.compile('<.*?>')
+
+    for current_ad in list_of_adverts:
+        data = []
+        filename = os.path.basename(current_ad)
+        data.append(filename)
 
         # Check if the file is one of the job adverts (which have
         # a set patern of filename
         if re.match(r'\w\w\w\d\d\d', filename):
 
-            with open(current_add, "r") as f:
+            with open(current_ad, "r") as f:
                 contents = f.read()
                 advert = BeautifulSoup(contents, 'lxml')
-                print(advert.h1)
+                advert_title = str(advert.h1).lower()
+                # Remove html
+                advert_title = re.sub(clean, '', advert_title)
+                data.append(advert_title)
 
-    return
+                try:
+                    date = advert.find('td', text='Placed on:').find_next_sibling('td').text
+                except:
+                    date = 0
+                    pass
+
+                try:
+                    try_date = advert.find('th', text='Placed On:').find_next_sibling('td').text
+                    # Only replace the date if the previous date is zero (i.e. don't overwrite
+                    # a valid date from the last 'try'
+                    if date == 0:
+                        date = try_date
+                except:
+                    pass
+
+                data.append(date)
+
+        big_data_list.append(data)
+
+    df = pd.DataFrame.from_records(big_data_list)
+    df.columns = ['filename', 'job title', 'date']
+
+    return df
+
+
+def find_rse(df):
+
+    df['rse'] = np.where(df['job title'].str.contains('research software engineer'), 'True', 'False')
+
+#    df['rse'] = False
+#    df['rse'][df['job title'].str.contains('research software engineer')] = 'True'
+
+    return df
 
 
 def main():
@@ -61,7 +115,15 @@ def main():
 
     list_of_adverts = find_files()
 
-    job_titles = read_html(list_of_adverts)
+    df = read_html(list_of_adverts)
+
+    df = find_rse(df)
+
+    #print(df)
+    #print(len(df))
+    print(df['rse'].value_counts())
+
+    export_to_csv(df, './', 'processed_jobs', False)
 
 if __name__ == '__main__':
     main()
